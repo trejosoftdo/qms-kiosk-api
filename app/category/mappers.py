2523d5cd150b37f109628392b72486e7f5502423
Victor Trejo <vic3jo@gmail.com>
2024-01-31 08:50:28 -0400

    Finish integration with core api

--- app/category/mappers.py
+++ app/category/mappers.py
@@ -2,6 +2,14 @@ from . import models
 
 
 def map_status(item: dict) -> models.Status:
+  """Maps a status item from the given data
+
+  Args:
+      item (dict): Status data
+
+  Returns:
+      models.Status: Mapped status
+  """
   return models.Status(
     id = item.get('id'),
     name = item.get('name'),
@@ -11,6 +19,14 @@ def map_status(item: dict) -> models.Status:
   )
 
 def map_category(item: dict) -> models.Category:
+  """Maps a category item from the given data
+
+  Args:
+      item (dict): category data
+
+  Returns:
+      models.Category: Mapped category
+  """
   return models.Category(
     id = item.get('id'),
     name = item.get('name'),
@@ -21,6 +37,14 @@ def map_category(item: dict) -> models.Category:
   )
 
 def map_category_service(item: dict) -> models.CategoryService:
+  """Maps a category service from the given data
+
+  Args:
+      item (dict): service data
+
+  Returns:
+      models.CategoryService: Mapped category service
+  """
   return models.CategoryService(
     id = item.get('id'),
     name = item.get('name'),
--- app/service/models.py
+++ app/service/models.py
@@ -3,6 +3,15 @@ from pydantic import BaseModel
 class CreateServiceTurnPayload(BaseModel):
     customerName: str
 
+class CommonHeaders(BaseModel):
+    authorization: str
+    application: str
+
+class CreateServiceTurnRequest(BaseModel):
+    serviceId: int
+    headers: CommonHeaders
+    payload: CreateServiceTurnPayload
+
 class CreateServiceTurnResponse(BaseModel):
     id: int
     customerName: str
